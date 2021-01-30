ca4ac5744f3a4d536951eafbee4dd2d6ee01d219
CoderOfTomorrow <nicolae.sb15@gmail.com>
2021-01-30 19:53:59 +0200

    //added a simple local database class for users based on Singletone Pattern

---
+++ Helpers/Database.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Bank.Models;
+
+namespace Bank.Helpers
+{
+    class Database
+    {
+        private Database() { }
+
+        private static Database _instance = null;
+        private static readonly object _lock = new object();
+        public List<User> UserContext { get; set; }
+
+        public static Database GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Database {
+                            UserContext = new List<User>()
+                        };
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public void AddUser(User newUser)
+        {
+            UserContext.Add(newUser);
+        }
+    }
+}
+
