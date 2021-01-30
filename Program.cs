97b9514527d8055e896e625cbefdd368d1c72118
CoderOfTomorrow <nicolae.sb15@gmail.com>
2021-01-30 21:20:32 +0200

    //added a simple menu

--- Program.cs
+++ Program.cs
@@ -1,24 +1,76 @@
 ﻿using System;
 using Bank.Services;
+using Bank.Helpers;
 
 namespace Bank
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             WalletService walletService = new WalletService();
             UserService userService = new UserService(walletService);
 
-            Console.WriteLine("Do you want to create a new user <YES> or <NO> ?");
-            var response = Console.ReadLine();
+            string option = null;
 
-            if(response == "YES" || response == "yes" || response == "Yes")
+            while (option != "0")
             {
                 Console.Clear();
-                var newUser = userService.Register();
+                Console.WriteLine("Choose a task :");
+                Console.WriteLine("1. Single Responsibility Principle");
+                Console.WriteLine("2. Singleton Patter");
+                Console.WriteLine("0. Exit");
+                Console.Write("\nChoose - ");
+                option = Console.ReadLine();
                 Console.Clear();
-                userService.ShowUserInfo(newUser);
+
+                //Single Responsability Principle
+                if (option == "1")
+                {
+                    Console.WriteLine("Do you want to create a new user ? <YES> or <NO>");
+                    var respons = Console.ReadLine();
+                    if(respons == "yes" || respons == "YES" || respons == "Yes")
+                    {
+                        Console.Clear();
+                        var newUser = userService.Register();
+                        Console.Clear();
+                        userService.ShowUserInfo(newUser);
+                        Console.ReadKey();
+                    }
+
+                }
+                //Singleton Pattern
+                else if (option == "2")
+                {
+                    Console.WriteLine("Do you want to create a new user ? <YES> or <NO>");
+                    var respons = Console.ReadLine();
+                    if (respons == "yes" || respons == "YES" || respons == "Yes")
+                    {
+                        Console.Clear();
+                        var newUser = userService.Register();
+                        
+                        //we will add the new user to a first instante of SingletonDatabase class
+                        var firstContext = SingletonDatabase.GetInstance();
+                        firstContext.AddUser(newUser);
+
+                        //then we will try to create a second instance
+                        var secondContext = SingletonDatabase.GetInstance();
+                        Console.Clear();
+
+                        //if firstContext is the same as secondContext then our SingeltonDatabase works fine
+                        if(firstContext == secondContext)
+                        { 
+                            Console.WriteLine("Singleton works, both variables contain the same instance.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Singleton failed, variables contain different instances.");
+                        }
+                        Console.ReadKey();
+                    }
+                }
+                else
+                    break;
             }
         }
     }
